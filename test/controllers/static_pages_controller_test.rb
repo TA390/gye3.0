16ae2c1933fe1096b9b4f1347c73a809819af1b6
Taha <taha_016@hotmail.co.uk>
2015-03-02 21:31:21 +0000

    updated test suite

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,24 +9,28 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Give Your Effort"
   end
   
   # About Us Page
   test "should get aboutUs" do
     get :aboutUs
     assert_response :success
+    assert_select "title", "About GYE"
   end
   
   # Events Page
   test "should get events" do
     get :events
     assert_response :success
+    assert_select "title", "Events"
   end
   
   # Volunteer Page
   test "should get volunteer" do
     get :volunteer
     assert_response :success
+    assert_select "title", "Volunteer"
   end
 
 end

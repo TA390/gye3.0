e4492714e8bbdd7b49b84606d5d2c1c8ced75a0d
Taha <taha_016@hotmail.co.uk>
2015-03-02 21:35:11 +0000

    HTML page layout v2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   
-  get 'static_pages/home', as: 'static_pages'
+  get 'static_pages/home', as: 'static_pages_home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

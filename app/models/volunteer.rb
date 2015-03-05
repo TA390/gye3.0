fc44e70b2b7db5180f51ccd438707cf0c33204cf
Taha <taha_016@hotmail.co.uk>
2015-03-05 14:32:29 +0000

    merged volunteer.rb

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,31 +1,35 @@
 class Volunteer < ActiveRecord::Base
-<<<<<<< HEAD
+
   has_many :events, through: :event_volunteers
-  validates :email, email_format: { message: "Error: Please enter a valid email address" }
+  
+  # enter all emails in a lowercase format
+    before_save { self.email = email.downcase }
+  
+  validates :email, 
+                 email_format: { message: "Error: Please enter a valid email address" }, 
+                  uniqueness: { case_sensitive: false },
+                  presence: true,
+                  length: {maximum: 254}
+  
+  
+  validates :first_name, 
+                presence: true,
+                length: {maximum: 254}
+  
+  validates :last_name, 
+                presence: true,
+                length: {maximum: 254}
+  
   #validates :email,
   # uniqueness: true { message: "Error: Email already in use" }
-  validates :name, length: { minimum: 2 }
-  validates :gender, presence: true
+  #validates :name, length: { minimum: 2 }
+  #validates :gender, presence: true
   #validates :password, length: { in: 6..20 }
   #validates :bio, length: { maximum: 500 }
   #validates :terms_of_service, acceptance: true
 end
-=======
   
-    # enter all emails in a lowercase format
-    before_save { self.email = email.downcase }
+    
   
-    validates :first_name, 
-                presence: true,
-                length: {maximum: 254}
-  
-    validates :last_name, 
-                presence: true,
-                length: {maximum: 254}
-  
-    validates :email, 
-                presence: true,
-                length: {maximum: 254},
-                uniqueness: { case_sensitive: false }
-end
->>>>>>> user_sign_up_v1
+
+

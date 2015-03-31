ec2df2b9df5775c233a06433f610c82b1f72037c
Taha <taha_016@hotmail.co.uk>
2015-03-31 12:49:02 +0000

    volunteer 'my events' page

--- app/controllers/event_volunteers_controller.rb
+++ app/controllers/event_volunteers_controller.rb
@@ -4,23 +4,27 @@ class EventVolunteersController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
     current_user.sign_up(@event)
-    
+ 
     respond_to do |format|
       format.html { redirect_to @event }
       format.js
     end
+    
+    flash[:success] = "Thank you for joining our event"
+    
   end
 
   def destroy
     @event = EventVolunteer.find(params[:id]).event
     current_user.opt_out(@event)
     
-    flash[:success] = "You are no longer subscribed to the event"
-    
     respond_to do |format|
       format.html { redirect_to @event }
       format.js
     end
+    
+    flash[:success] = "You are no longer subscribed to #{@event.name}"
+    
   end
   
 end
